ce86307813e44a354b48a1b1cf4efd6ebd03f35c
Aayush Anand <aaayushanand5@gmail.com>
2021-06-19 14:53:35 +0530

    Rendering Torus (completed till Sec 6.2)

--- LOGL/src/Application.cpp
+++ LOGL/src/Application.cpp
@@ -14,14 +14,14 @@
 
 #include "Utils.h"
 #include "Sphere.h"
-#include "Application.h"
+#include "Torus.h"
 
 #define numVAOs 1
-#define numVBOs 9
+#define numVBOs 4
 
 float cameraX, cameraY, cameraZ;
 float cubeLocX, cubeLocY, cubeLocZ;
-float sphereLocX, sphereLocY, sphereLocZ;
+float torusLocX, torusLocY, torusLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
@@ -33,65 +33,54 @@ GLuint mvLoc, projLoc;
 int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;
-GLuint sunTexture, earthTexture, moonTexture;
+GLuint brickTexture;
 
 
 GLuint mLoc, vLoc, tfLoc; //The uniform location for the model and view matrix and the time factor
-Sphere sun(48), earth(48), moon(48);
 
-void FillVertsAndIndices(std::vector<float>& pvalues, std::vector<float>& tvalues, std::vector<float>& nvalues)
-{
-	std::vector<int> ind = earth.GetIndices();
-	const std::vector<glm::vec3>& earthVert = earth.GetVertices();
-	const std::vector<glm::vec2>& eartTex = earth.GetTexCoords();
-	const std::vector<glm::vec3>& earthNorm = earth.GetNormals();
+Sphere mySphere(48);
+Torus myTorus(0.5f, 0.2f, 48);
 
-	int numIndices = earth.GetNumIndices();
-	for (int i = 0; i < numIndices; i++) {
-		pvalues.push_back((earthVert[ind[i]]).x);
-		pvalues.push_back((earthVert[ind[i]]).y);
-		pvalues.push_back((earthVert[ind[i]]).z);
-		tvalues.push_back((eartTex[ind[i]]).s);
-		tvalues.push_back((eartTex[ind[i]]).t);
-		nvalues.push_back((earthNorm[ind[i]]).x);
-		nvalues.push_back((earthNorm[ind[i]]).y);
-		nvalues.push_back((earthNorm[ind[i]]).z);
+void setupVertices(void) {
+	std::vector<int> ind = myTorus.getIndices();
+	std::vector<glm::vec3> vert = myTorus.getVertices();
+	std::vector<glm::vec2> tex = myTorus.getTexCoords();
+	std::vector<glm::vec3> norm = myTorus.getNormals();
+	std::vector<float> pvalues;
+	std::vector<float> tvalues;
+	std::vector<float> nvalues;
+	int numVertices = myTorus.getNumVertices();
+	for (int i = 0; i < numVertices; i++) {
+		pvalues.push_back(vert[i].x);
+		pvalues.push_back(vert[i].y);
+		pvalues.push_back(vert[i].z);
+		tvalues.push_back(tex[i].s);
+		tvalues.push_back(tex[i].t);
+		nvalues.push_back(norm[i].x);
+		nvalues.push_back(norm[i].y);
+		nvalues.push_back(norm[i].z);
 	}
-}
-
-void setupVertices(void)
-{
-	std::vector<float> pvalues; // vertex positions
-	std::vector<float> tvalues; // texture coordinates
-	std::vector<float> nvalues; // normal vectors
-
-
-	//Sun
-	FillVertsAndIndices(pvalues, tvalues, nvalues);
 	glGenVertexArrays(1, vao);
 	glBindVertexArray(vao[0]);
-	glGenBuffers(3, vbo);
-	// put the vertices into buffer #0
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, pvalues.size() * sizeof(float), &pvalues[0], GL_STATIC_DRAW);
-	// put the texture coordinates into buffer #1
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, tvalues.size() * sizeof(float), &tvalues[0], GL_STATIC_DRAW);
-	// put the normals into buffer #2
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
-	glBufferData(GL_ARRAY_BUFFER, nvalues.size() * sizeof(float), &nvalues[0], GL_STATIC_DRAW);
-
+	glGenBuffers(4, vbo); // generate VBOs as before, plus one for indices
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // vertex positions
+	glBufferData(GL_ARRAY_BUFFER, pvalues.size() * 4, &pvalues[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // texture coordinates
+	glBufferData(GL_ARRAY_BUFFER, tvalues.size() * 4, &tvalues[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[2]); // normal vectors
+	glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]); // indices
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, ind.size() * 4, &ind[0], GL_STATIC_DRAW);
 }
+
 
 void init(GLFWwindow* window) 
 {
 	renderingProgram = Utils::CreateShaderProgram();
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 10.0f;
-	sphereLocX = 0.0f; sphereLocY = 0.0f; sphereLocZ = 0.0f;
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 2.0f;
+	torusLocX = 0.0f; torusLocY = 0.0f; torusLocZ = 0.0f;
 	setupVertices();
-	sunTexture = Utils::LoadTexture("Textures/Sun.jpg");
-	earthTexture = Utils::LoadTexture("Textures/Earth.jpg");
-	moonTexture = Utils::LoadTexture("Textures/Moon.jpg");
+	brickTexture = Utils::LoadTexture("Textures/Brick.jpg");
 }
 
 void Display(GLFWwindow* window, double currentTime)
@@ -99,7 +88,7 @@ void Display(GLFWwindow* window, double currentTime)
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(renderingProgram);
-	
+
 	// get the uniform variables for the MV and projection matrices
 	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
@@ -112,22 +101,15 @@ void Display(GLFWwindow* window, double currentTime)
 
 	// pass on the view matrix and the model matrix for the pyramid (after applying some rotation and scale transforms)
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-	
-	std::stack<glm::mat4> mvStack;
-	mvStack.push(vMat);
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(torusLocX, torusLocY, torusLocZ));
+	mMat *= glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(1.0f, 1.0f, 1.0f));
+
+	mvMat = vMat * mMat;
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 
-	//Sun
-	mvStack.push(mvStack.top());
-	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(sphereLocX, sphereLocX, sphereLocZ));
-	mvStack.push(mvStack.top());
-	mvStack.top() *= glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
-	mvStack.top() *= glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 2.0f));
-
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
-
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
@@ -136,57 +118,11 @@ void Display(GLFWwindow* window, double currentTime)
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(1);
 
-	glBindTexture(GL_TEXTURE_2D, sunTexture);
+	glBindTexture(GL_TEXTURE_2D, brickTexture);
 	glActiveTexture(GL_TEXTURE0);
-	
-	glDrawArrays(GL_TRIANGLES, 0, sun.GetNumIndices());
-	mvStack.pop();	//pop the rotation transform applied matrix
 
-	//Earth
-	mvStack.push(mvStack.top());
-	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(6.0f * sin((float)currentTime), 0.0f, 6.0f * cos((float)currentTime)));
-	mvStack.push(mvStack.top());
-	mvStack.top() *= glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(-1.0f, 1.0f, 0.0f));
-
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, earthTexture);
-
-	glDrawArrays(GL_TRIANGLES, 0, earth.GetNumIndices());
-
-	mvStack.pop();	//pop the rotation transform applied matrix
-
-	//Moon
-	mvStack.push(mvStack.top());
-	mvStack.top() *= glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 2.0f * cos((float)currentTime), 2.0f * sin((float)currentTime)));
-	mvStack.top() *= glm::rotate(glm::mat4(1.0f), (float) currentTime, glm::vec3(0.0f, 1.0f, 1.0f));
-	mvStack.top() *= glm::scale(glm::mat4(1.0f), glm::vec3(0.25f, .25f, .25f));
-
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, moonTexture);
-
-	glDrawArrays(GL_TRIANGLES, 0, moon.GetNumIndices());
-
-	mvStack.pop(); mvStack.pop(); mvStack.pop(); mvStack.pop();
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
+	glDrawElements(GL_TRIANGLES, myTorus.getNumIndices(), GL_UNSIGNED_INT, 0);
 }
 
 int main()
@@ -196,7 +132,7 @@ int main()
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello, World!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, World!", NULL, NULL);
 	if (window == NULL)
 	{
 		fprintf(stderr, "Could not create window!\n");
--- LOGL/src/Application.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void FillVertsAndIndices(std::vector<float>& pvalues, std::vector<float>& tvalues, std::vector<float>& nvalues);
